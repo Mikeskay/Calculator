99371f4a6efa6b24e60b6b818e3905d2328d4587
Mikeskay <47972946+Mikeskay@users.noreply.github.com>
2022-03-21 18:09:06 +0000

    Create CalculatorTest.java

---
+++ test/CalculatorTest.java
@@ -0,0 +1,19 @@
+package calculator.test;
+
+import calculator.base.display.*;
+import calculator.base.engine.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        
+
+        CalculatorDisplay newCal = new CalculatorDisplay();
+
+      
+    }
+    
+}
